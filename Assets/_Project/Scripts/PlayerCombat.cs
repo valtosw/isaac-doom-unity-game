5038fca30f31a26b36beb2afdbcd277acd4cb7a6
valtosw <volodijbessarabov@gmail.com>
2026-04-25 18:35:20 +0300

    Add PlayerCombat script for weapon firing input handling

---
+++ Assets/_Project/Scripts/PlayerCombat.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets._Project.Scripts
+{
+    public class PlayerCombat : MonoBehaviour
+    {
+        [Header("Input")]
+        [SerializeField] private InputActionReference fireAction;
+
+        [Header("Equipped Weapon")]
+        [SerializeField] private Weapon currentWeapon;
+
+        private void OnEnable()
+        {
+            fireAction.action.Enable();
+            fireAction.action.performed += OnFirePerformed;
+        }
+
+        private void OnDisable()
+        {
+            fireAction.action.performed -= OnFirePerformed;
+            fireAction.action.Disable();
+        }
+
+        private void OnFirePerformed(InputAction.CallbackContext context)
+        {
+            if (currentWeapon != null && currentWeapon.CanAttack())
+            {
+                currentWeapon.Attack();
+            }
+        }
+
+        public void EquipWeapon(Weapon weapon)
+        {
+            currentWeapon = weapon;
+        }
+    }
+}
