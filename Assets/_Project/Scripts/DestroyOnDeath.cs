cab7d3e65a52fad0a039ae601f4c220c30cd83e1
valtosw <volodijbessarabov@gmail.com>
2026-04-25 18:34:50 +0300

    Add DestroyOnDeath script to handle GameObject destruction

---
+++ Assets/_Project/Scripts/DestroyOnDeath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+	public class DestroyOnDeath: MonoBehaviour
+	{
+		public void HandleDeath()
+		{
+			Destroy(gameObject);
+        }
+	}
+}
