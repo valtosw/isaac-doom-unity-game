44d59bf87d5bfdc88a1e2b19e997fee2ea552649
valtosw <volodijbessarabov@gmail.com>
2026-04-25 13:10:02 +0300

    Refactor movement scripts

--- Assets/_Project/Scripts/PlayerMove.cs
+++ Assets/_Project/Scripts/PlayerMove.cs
@@ -1,81 +1,84 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
-public class PlayerMove : MonoBehaviour
+namespace Assets._Project.Scripts
 {
-    private static readonly int IsWalkingHash = Animator.StringToHash("IsWalking");
-
-    [Header("Input")]
-    [SerializeField] private InputActionReference moveAction;
-
-    [Header("References")]
-    [SerializeField] private Animator cameraAnimator;
-
-    [Header("Settings")]
-    [SerializeField] private float playerSpeed = 5f;
-    [SerializeField] private float gravity = -9.81f;
-
-    private CharacterController _characterController;
-    private Vector3 _movementVector;
-    private float _verticalVelocity;
-    private bool _isWalking;
-
-    private void Awake()
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMove : MonoBehaviour
     {
-        _characterController = GetComponent<CharacterController>();
-    }
+        private static readonly int IsWalkingHash = Animator.StringToHash("IsWalking");
 
-    private void OnEnable()
-    {
-        moveAction.action.Enable();
-    }
+        [Header("Input")]
+        [SerializeField] private InputActionReference moveAction;
 
-    private void OnDisable()
-    {
-        moveAction.action.Disable();
-    }
+        [Header("References")]
+        [SerializeField] private Animator cameraAnimator;
 
-    private void Update()
-    {
-        CalculateMovement();
-        ApplyGravity();
-        MovePlayer();
-        CheckForHeadbob();
+        [Header("Settings")]
+        [SerializeField] private float playerSpeed = 5f;
+        [SerializeField] private float gravity = -9.81f;
 
-        if (cameraAnimator != null)
+        private CharacterController _characterController;
+        private Vector3 _movementVector;
+        private float _verticalVelocity;
+        private bool _isWalking;
+
+        private void Awake()
         {
-            cameraAnimator.SetBool(IsWalkingHash, _isWalking);
-        }
-    }
-
-    private void CalculateMovement()
-    {
-        var input = moveAction.action.ReadValue<Vector2>();
-        var inputVector = new Vector3(input.x, 0, input.y);
-
-        _movementVector = transform.TransformDirection(inputVector) * playerSpeed;
-    }
-
-    private void ApplyGravity()
-    {
-        if (_characterController.isGrounded && _verticalVelocity < 0)
-        {
-            _verticalVelocity = -2f;
+            _characterController = GetComponent<CharacterController>();
         }
 
-        _verticalVelocity += gravity * Time.deltaTime;
-        _movementVector.y = _verticalVelocity;
-    }
+        private void OnEnable()
+        {
+            moveAction.action.Enable();
+        }
 
-    private void MovePlayer()
-    {
-        _characterController.Move(_movementVector * Time.deltaTime);
-    }
+        private void OnDisable()
+        {
+            moveAction.action.Disable();
+        }
 
-    private void CheckForHeadbob()
-    {
-        var horizontalVelocity = new Vector3(_characterController.velocity.x, 0, _characterController.velocity.z);
-        _isWalking = horizontalVelocity.magnitude > 0.1f;
+        private void Update()
+        {
+            CalculateMovement();
+            ApplyGravity();
+            MovePlayer();
+            CheckForHeadbob();
+
+            if (cameraAnimator != null)
+            {
+                cameraAnimator.SetBool(IsWalkingHash, _isWalking);
+            }
+        }
+
+        private void CalculateMovement()
+        {
+            var input = moveAction.action.ReadValue<Vector2>();
+            var inputVector = new Vector3(input.x, 0, input.y);
+
+            _movementVector = transform.TransformDirection(inputVector) * playerSpeed;
+        }
+
+        private void ApplyGravity()
+        {
+            if (_characterController.isGrounded && _verticalVelocity < 0)
+            {
+                _verticalVelocity = -2f;
+            }
+
+            _verticalVelocity += gravity * Time.deltaTime;
+            _movementVector.y = _verticalVelocity;
+        }
+
+        private void MovePlayer()
+        {
+            _characterController.Move(_movementVector * Time.deltaTime);
+        }
+
+        private void CheckForHeadbob()
+        {
+            var horizontalVelocity = new Vector3(_characterController.velocity.x, 0, _characterController.velocity.z);
+            _isWalking = horizontalVelocity.magnitude > 0.1f;
+        }
     }
 }
