273efbeac458434ba0a051cb3d810df2a3405bfb
valtosw <volodijbessarabov@gmail.com>
2026-04-25 18:34:30 +0300

    Add base Weapon, MeleeWeapon, and RangedWeapon scripts

---
+++ Assets/_Project/Scripts/RangedWeapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+    public class RangedWeapon : Weapon
+    {
+        [Header("Ranged Settings")]
+        [SerializeField] private GameObject projectilePrefab;
+        [SerializeField] private Transform firePoint;
+
+        public override void Attack()
+        {
+            _lastAttackTime = Time.time;
+
+            var spawnedProjectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+
+            if (spawnedProjectile.TryGetComponent(out Projectile projectileComponent))
+            {
+                projectileComponent.Initialize(Damage);
+            }
+        }
+    }
+}
