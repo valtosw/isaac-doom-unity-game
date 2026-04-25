d93bb588c19057e5ca9fb100505452afcf673873
valtosw <volodijbessarabov@gmail.com>
2026-04-25 18:35:05 +0300

    Add KamikazeAttack script for self-destruct enemy attack

---
+++ Assets/_Project/Scripts/KamikazeAttack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+    [RequireComponent(typeof(Health))]
+    public class KamikazeAttack: MonoBehaviour
+	{
+        [Header("Settings")]
+        [SerializeField] private float damageAmount = 20f;
+
+        private Health _myHealth;
+
+        private void Awake()
+        {
+            _myHealth = GetComponent<Health>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player") || !other.TryGetComponent(out IDamageable damageableTarget))
+            {
+                return;
+            }
+
+            damageableTarget.TakeDamage(damageAmount);
+            Explode();
+        }
+
+        private void Explode()
+        {
+            if (_myHealth != null)
+            {
+                _myHealth.TakeDamage(_myHealth.MaxHealth);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
